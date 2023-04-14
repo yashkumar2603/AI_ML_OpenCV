b8f697142d16c8071a6e582ab81be4bab6335b4a
yashkumar2603 <yash.2603kumar@kgpian.iitkgp.ac.in>
2023-04-15 00:53:30 +0530

    changes

--- OpenCV_read_camera.py
+++ OpenCV_read_camera.py
@@ -10,10 +10,18 @@ def rescale(frame, scale):
 capture=cv.VideoCapture(0)
 while(True):
     isTrue, frame=capture.read()
-    new_frame = rescale(frame, 1)
-    cv.circle(new_frame, (300, 300), 150, (255, 255, 0), 15)
-    cv.putText(new_frame, 'HELLO', (250,250), cv.FONT_HERSHEY_TRIPLEX, 1.0, (255, 0, 255), 4)
-    cv.imshow('frame', new_frame)
+    new_frame = rescale(frame, 2)
+
+    #gray = cv.cvtColor(new_frame, cv.COLOR_BGR2GRAY)
+
+    #cv.circle(gray, (300, 300), 150, (255, 255, 0), 15)
+
+    #cv.putText(gray, 'HELLO', (250,250), cv.FONT_HERSHEY_TRIPLEX, 1.0, (255, 0, 255), 4)
+
+    blur=cv.GaussianBlur(new_frame, (5,5), cv.BORDER_DEFAULT)
+    canny=cv.Canny(blur, 125, 175)
+
+    cv.imshow('frame', canny)
     if cv.waitKey(20) & 0xFF==ord('q'):
         break
 
